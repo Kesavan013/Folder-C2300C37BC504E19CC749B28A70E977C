583ae7ae2c2e3cd9aa33caaa3cb9d139500aa9bb
Kesavan013 <144314630+Kesavan013@users.noreply.github.com>
2023-09-21 13:09:46 +0530

    challenge 3.1.py

--- challenge 3.1.py
+++ challenge 3.1.py
@@ -3,7 +3,7 @@ Write a function called linear_search_product that takes the list of products an
 name as input. The function should perform a linear search to find the target product in the list and
 return a list of indices of all occurrences of the product if found, or an empty list if the product is not
 found.
-"""
+""
 
 
 def linearSearchProduct(productList, targetProduct):
